66591227c5d2eeb97b3f8c7e4a47be8af26b9d7e
AdHoc2675 <21900741@handong.ac.kr>
2022-12-08 10:51:50 +0900

    updated

--- lib/Pages/profilepage.dart
+++ lib/Pages/profilepage.dart
@@ -32,11 +32,25 @@ class _MyProfilePageState extends State<MyProfilePage> {
               height: 50,
             ),
             Center(
-              child: Text('Flash',
-                  style: Timetravel(
-                    50,
-                    53.25,
-                  )),
+              child: SizedBox(
+                height: 60,
+                child: AnimatedTextKit(repeatForever: true, animatedTexts: [
+                  FlickerAnimatedText("Flash",
+                      textStyle: TextStyle(
+                          fontSize: 50,
+                          fontFamily: 'Timetravle',
+                          fontWeight: FontWeight.w500,
+                          height: 53.25 / 50,
+                          color: ReturnColor('white'),
+                          shadows: [
+                            Shadow(
+                              blurRadius: 7.0,
+                              color: Colors.white,
+                              offset: Offset(2, 2),
+                            ),
+                          ]))
+                ]),
+              ),
             ),
             SizedBox(
               height: 40,
@@ -69,7 +83,7 @@ class _MyProfilePageState extends State<MyProfilePage> {
                           fixedSize: Size(330, 80)),
                       onPressed: (() {}),
                       child: Text(
-                        'User1234',
+                        'Chat',
                         style: ABeeZee(30, 37.82, color: 'black'),
                       ),
                     ),
@@ -77,72 +91,6 @@ class _MyProfilePageState extends State<MyProfilePage> {
                 ),
               ),
               SizedBox(
-                height: 8,
-              ),
-              Stack(
-    return Center(
-      child: ListView(
-        children: <Widget>[
-          SizedBox(
-            height: 50,
-          ),
-          Center(
-            child: SizedBox(
-              height: 60,
-              child: AnimatedTextKit(repeatForever: true, animatedTexts: [
-                FlickerAnimatedText("Flash",
-                    textStyle: TextStyle(
-                        fontSize: 50,
-                        fontFamily: 'Timetravle',
-                        fontWeight: FontWeight.w500,
-                        height: 53.25 / 50,
-                        color: ReturnColor('white'),
-                        shadows: [
-                          Shadow(
-                            blurRadius: 7.0,
-                            color: Colors.white,
-                            offset: Offset(2, 2),
-                          ),
-                        ]))
-              ]),
-            ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          Container(
-            width: 150,
-            height: 150,
-            decoration: new BoxDecoration(
-                shape: BoxShape.circle,
-                image: new DecorationImage(
-                    fit: BoxFit.cover,
-                    image: NetworkImage("https://i.imgur.com/BoN9kdC.png"))),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Column(children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 0, horizontal: 30),
-              child: Stack(
-                children: [
-                  ElevatedButton(
-                    style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40)),
-                        side: BorderSide(width: 0, color: ReturnColor('white')),
-                        backgroundColor: ReturnColor('white'),
-                        fixedSize: Size(330, 80)),
-                    onPressed: (() {}),
-                    child: Text(
-                      'Chat',
-                      style: ABeeZee(30, 37.82, color: 'black'),
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(
                 height: 120,
               ),
               Stack(
